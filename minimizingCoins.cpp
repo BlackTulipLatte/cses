bfa4179a2fbb38f7dc45736ce71c5f67c78c2ab2
BlackTulipLatte <86974706+BlackTulipLatte@users.noreply.github.com>
2023-08-22 13:22:13 -0400

    minimize coins

--- minimizingCoins.cpp
+++ minimizingCoins.cpp
@@ -5,13 +5,24 @@ int main(){
 
     int coins, target;
     int numberCoins = 0;
-    cin >> coins;
-    cin >> target;
+    cin >> coins >> target;
 
     int coinValues[coins];
+    long long numberCoins[target + 1];
 
     for(int i = 0; i < coins; i++){
         cin >> coinValues[i];
+    }
+
+    numberCoins[0] = 0;
+
+    for(int i = 1; i < target; i++){
+        numberCoins[i] = 1000000000;
+        for(int j = 0; j < coins; j++){
+            if(i >= coinValues[j]){
+                numberCoins[i] = min(numberCoins[i], numberCoins[i - coinValues[j]] + 1);
+            }
+        }
     }
 
 
